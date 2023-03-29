2b751b4b1c5d62bf12da057c517c89f2e7416f1a
mrayandika15 <mrayandika15@gmail.com>
2023-03-30 01:23:51 +0700

    fix(): breakpoints, font size, etc

--- src/components/HeroSection/MottoSection.tsx
+++ src/components/HeroSection/MottoSection.tsx
@@ -5,13 +5,13 @@ const MottoSection: React.FC = () => {
   return (
     <Flex w="full" position="relative" direction="column">
       <Flex direction="column" gap="10px">
-        <Heading fontSize={["58px", "140px"]} fontWeight="300">
+        <Heading fontSize="clamp(250% , 10vw , 140px)" fontWeight="300">
           WE CREATE
         </Heading>
-        <Heading fontSize={["58px", "140px"]} fontWeight="300">
+        <Heading fontSize="clamp(250% , 10vw  , 140px)" fontWeight="300">
           CREATIVE
         </Heading>
-        <Heading fontSize={["58px", "140px"]} fontStyle="italic">
+        <Heading fontSize="clamp(250% , 10vw  , 140px)" fontStyle="italic">
           INOVATIONS
         </Heading>
       </Flex>
@@ -20,10 +20,10 @@ const MottoSection: React.FC = () => {
         w={["full", "540px"]}
         h={["355px", "540px"]}
         bg="gray.300"
-        position={["absolute", "absolute"]}
-        right={["0", "32"]}
+        position={["relative", "absolute"]}
+        right={["0", "120px"]}
         zIndex="hide"
-        bottom={["-320px", "12"]}
+        bottom={["25px", "12"]}
       >
         <video src="./assets/video/Hero.mp4" loop autoPlay={true} muted />
       </AspectRatio>
